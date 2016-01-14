fff87c4f8df485cd4af74c7525cd404018c29856
Marius Balaj <balajmarius93@gmail.com>
2016-01-14 14:27:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	
-	workDuration: 5000, /* 25 min * 6000 ms */
-	pauseDuration: 1000, /* 25 min * 6000 ms */
+	workDuration: 25 * 6000, /* 25 min * 6000 ms */
+	pauseDuration: 5 * 6000, /* 25 min * 6000 ms */
 	
 	startText: ':white_check_mark: _you should get *back to work*!_', /* supports slack markdown */
 	pauseText: ':bell: _you deserve a *break*!_',
